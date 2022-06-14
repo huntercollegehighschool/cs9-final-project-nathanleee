a6a170a1a6e39d03179d26d68d51546fd600a310
Nathan Lee <103280927+nathanleee@users.noreply.github.com>
2022-06-14 03:45:24 +0000

    main

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 """
-Name(s):
-Name of Project:
+Name(s): Daniel Kim & Nathan Lee
+Name of Project: Hangman Game with Python
 """
 
-#Write the main part of your program here. Use of the other pages is optional.
+print("Hello there! Lets play a game of Hangman! Instead of using words, we will be using country names. Here are the rules:")
 
-#import page1  # uncomment if you're using page1
-#import page2  # uncomment if you're using page2
-#import page3  # uncomment if you're using page3
-#import page4  # uncomment if you're using page4
+print("1. Not all countries are going by their name, but rather their better known name. For example, Holy See, which you may know as Vatican City will be labeled Vatican City.")
+
+print("2. You will get seven tries to guess the country. Each try, you can guess a letter, a dash, or and apostrophe (for example, in Guinea-Bissau or Cote d'Ivoire).")
+
+print("Let's play!")
+
+import random 
+from page1 import list
+
+
+def playhangman(country):
+
+
+  
+
+def play():
+  country = random.choice(list)
+  playhangman(country)
+    
